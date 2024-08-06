b9fcb180725f531ce27f732f155179f70b6aeeea
Martin <mscazzola@fi.uba.ar>
2024-08-05 21:15:00 -0300

    fix
    Co-authored-by: brandonschiffer1 <brandonschiffer11@gmail.com>

--- control/routers/matching.py
+++ control/routers/matching.py
@@ -117,9 +117,6 @@ def get_candidates(job_id: str, k: int = 10):
         print("data cruda", ' '.join(jd_data['requirements']))
         print("data limpia", TextCleaner(' '.join(jd_data['requirements'])).clean_text())
 
-        # Job title match weight
-        job_title_weight = 1.5 if jd_data['title'] in resume_fields["job_titles"] else 1.0
-
         requirements_skills = DataExtractor(' '.join(jd_data['requirements'])).extract_skills()
         print("requirements_skills", requirements_skills)
         
@@ -134,6 +131,9 @@ def get_candidates(job_id: str, k: int = 10):
             )
             
             resume_fields = resume_fields.json()
+
+            # Job title match weight
+            job_title_weight = 1.5 if jd_data['title'] in resume_fields["job_titles"] else 1.0
 
             # Requirements match weight
             requirements_skills_weight = 1.0
