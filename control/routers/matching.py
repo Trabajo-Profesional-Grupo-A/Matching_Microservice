cc1f2c1968dea7e4daa625058b86aaab0f8245ce
Martin <mscazzola@fi.uba.ar>
2024-09-25 19:56:32 -0300

    cv contra jds
    Co-authored-by: lara-scazzola1 <lscazzola@fi.uba.ar>
    
    Co-authored-by: brandonschiffer1 <brandonschiffer11@gmail.com>

--- control/routers/matching.py
+++ control/routers/matching.py
@@ -15,6 +15,7 @@ from concurrent.futures import ThreadPoolExecutor
 import threading
 
 executor = ThreadPoolExecutor(max_workers=1)
+executor2 = ThreadPoolExecutor(max_workers=1)
 
 import requests
 from fastapi import (
@@ -30,6 +31,8 @@ API_USERS_URL="https://users-microservice-mmuh.onrender.com"
 API_COMPANIES_URL="https://companies-microservice.onrender.com"
 
 API_LOCATION_KEY = "pk.16ff76ca0f92be97f397a3683dae4e14"
+AMOUNT_COMPANIES_CHECK = 3
+
 
 cred = credentials.Certificate("firebase_credentials.json")
 firebase_admin.initialize_app(cred, {"storageBucket": "tpp-grupoa.appspot.com"})
@@ -55,6 +58,20 @@ def retrain_in_background():
 
     model = Doc2Vec.load('./models/cv_job_maching_vector_size_10_min_count_5_window_3_epochs_50.model')  # Carga el nuevo modelo
 
+def check_new_match(user_email):
+    job_ids = index_jd.fetch(ids=[], namespace="ns1")["vectors"].keys()
+
+    print("job_ids", job_ids)
+        
+    for job_id in job_ids:
+        top_candidates = get_top_candidates(job_id, AMOUNT_COMPANIES_CHECK)
+
+        if top_candidates[0] == user_email:
+            print("Match encontrado")
+            
+    return
+
+
 
 @router.post("/matching/candidate/{user_email}/")
 def upload_candidate(user_email: str, user_model_data: ModelData):
@@ -68,7 +85,9 @@ def upload_candidate(user_email: str, user_model_data: ModelData):
             ],
             namespace="ns1"
         )
-    
+
+        executor2.submit(check_new_match(user_email))
+
     except Exception as error:
         raise HTTPException(status_code=BAD_REQUEST, detail=str(error)) from error
     return {"message": "Candidate uploaded successfully"}
@@ -119,175 +138,179 @@ def upload_job(job_id: str, job_description: JobDescription):
 @router.get("/matching/candidate/{job_id}/", response_model=List[str])
 def get_candidates(job_id: str, k: int = 10):
     try:
-        job_vector = index_jd.fetch(ids=[job_id], namespace="ns1")["vectors"].get(job_id)["values"]
-
-        n = round(2 * k, 0)
-
-        candidates = index_cv.query(vector=job_vector, include_values = True, top_k=int(n), namespace="ns1")
-
-        ids = {}
-        for candidate in candidates.get("matches"):
-            ids[candidate["id"]] = candidate["score"]
-
-        url = API_COMPANIES_URL + f"/companies/company/job_description_to_match/{job_id}/"
-        jd_data = requests.get(
-            url
-        )
-
-        jd_data = jd_data.json()
-
-        score_list = {}
-
-        dict_jd = JobDescriptionProcessor(' '.join([jd_data["title"], jd_data["description"], ' '.join(jd_data["responsabilities"]), ' '.join(jd_data["requirements"])])).process()
-
-        #print("job description: ", dict_jd)
-
-        jd_data["pos_frequencies"] = dict_jd["pos_frequencies"]
-        jd_data["keyterms"] = dict_jd["keyterms"]
-        jd_data["keywords_tfidf"] = dict_jd["keywords_tfidf"]
-
-        requirements_skills = DataExtractor(' '.join(jd_data['requirements'])).extract_skills()
-        
-        requirements_education = DataExtractor(' '.join(jd_data['requirements'])).extract_education_title()
-
-        print("requirements_skills: ", requirements_skills)
-        print("requirements_education: ", requirements_education)
-
-        for email, score in ids.items():
-            print("Candidato: ", email)
-            print("Score inicial: ", score)
-
-            url = API_USERS_URL + f"/users/user/resume/{email}/"
-            resume_fields = requests.get(
-                url
-            )
-            
-            resume_fields = resume_fields.json()
-
-            #print("resume_fields del candidato", resume_fields)
-            print("job_titles jd: ", jd_data['title'])
-            print("job titles resume: ", resume_fields["job_titles"])
-
-            # Job title match weight
-            job_title_weight = 1.2 if jd_data['title'].lower() in resume_fields["job_titles"] else 1.0
-
-            print("job_title_weight: ", job_title_weight)
-
-            # Requirements match weight
-            requirements_skills_weight = 1.0
-            print("skills: ", resume_fields["skills"])
-            print("model_data: ", resume_fields["model_data"])
-
-            for req in requirements_skills:
-                if req not in resume_fields["skills"] and req not in resume_fields["model_data"]:
-                    requirements_skills_weight -= 0.1
-                    if requirements_skills_weight < 0:
-                        requirements_skills_weight = 0
-                        break
-                elif req in resume_fields["skills"] or req in resume_fields["model_data"]:
-                    requirements_skills_weight += 0.1
-
-            print("requirements_skills_weight: ", requirements_skills_weight)
-
-            requirements_education_weight = 1.0
-
-            cleaned_education = TextCleaner(resume_fields["education"]).clean_text()
-
-            for req in requirements_education:
-                if req not in cleaned_education and req not in resume_fields["model_data"]:
-                    requirements_education_weight -= 0.1
-                    if requirements_education_weight < 0:
-                        requirements_education_weight = 0
-                        break
-                elif req in cleaned_education or req in resume_fields["model_data"]:
-                    requirements_education_weight += 0.1
-            
-            print("requirements_education_weight: ", requirements_education_weight)
-                    
-
-            # Pos frequencies weight
-            pos_freq_weight = 1.0
-            for word in jd_data['pos_frequencies'].keys():
-                if word in resume_fields["model_data"]:
-                    pos_freq_weight += 0.1
-            
-            print("pos_freq_weight: ", pos_freq_weight)
-
-            # Keyterms and keywords_tfidf weight
-            keyterms_weight = 1.0
-            for term in jd_data['keyterms']:
-                if term[0] in resume_fields["model_data"]:
-                    keyterms_weight += 0.1
-            
-            for keyword in jd_data['keywords_tfidf']:
-                if keyword in resume_fields["model_data"]:
-                    keyterms_weight += 0.1
-
-            print("keyterms_weight: ", keyterms_weight)
-
-            
-
-            location_weight = 1.0
-
-            if jd_data["work_model"] == "on-site":
-                company_coordinates = get_coordinates_locationiq(jd_data["address"], API_LOCATION_KEY)
-                user_coordinates = get_coordinates_locationiq(resume_fields["address"], API_LOCATION_KEY)
-                distance_km = geodesic(user_coordinates, company_coordinates).kilometers
-                if distance_km > 50:
-                    location_weight = 0.3
-                elif distance_km < 10:
-                    location_weight = 1.2
-            elif jd_data["work_model"] == "hybrid":
-                company_coordinates = get_coordinates_locationiq(jd_data["address"], API_LOCATION_KEY)
-                user_coordinates = get_coordinates_locationiq(resume_fields["address"], API_LOCATION_KEY)
-                distance_km = geodesic(user_coordinates, company_coordinates).kilometers
-                if distance_km > 50:
-                    location_weight = 0.6
-                elif distance_km < 10:
-                    location_weight = 1.1
-
-            print("location_weight: ", location_weight)
-
-
-            #calculate age weight
-            age_weight = 1.0
-            if resume_fields["age"] < jd_data["age_range"][0]:
-                age_weight -= ((jd_data["age_range"][0] - resume_fields["age"]) * 0.04)
-
-            elif resume_fields["age"] > jd_data["age_range"][1]:    
-                age_weight -= ((resume_fields["age"] - jd_data["age_range"][1]) * 0.04)
-            else:
-                age_weight = 1.2
-
-            print("age_weight: ", age_weight)
-
-            job_preference_weight = 1.3
-
-            print("job_preferences: ", resume_fields["job_preferences"])
-            
-            if resume_fields["job_preferences"]:
-                job_preference_weight = 1.3 if jd_data['title'].lower() in resume_fields["job_preferences"] else 0.5
-
-            print("job_preference_weight: ", job_preference_weight)
-
-            years_of_experience_weight = 1.0
-            if resume_fields["years_of_experience"] < jd_data["years_of_experience"]:
-                years_of_experience_weight -= ((jd_data["years_of_experience"] - resume_fields["years_of_experience"]) * 0.04)
-
-            # Calculate final weighted similarity score
-            final_similarity_score = score * job_title_weight * requirements_skills_weight * requirements_education_weight * pos_freq_weight * keyterms_weight * location_weight * age_weight * job_preference_weight * years_of_experience_weight
-            
-            print("final_similarity_score: ", final_similarity_score)
-            
-            score_list[email] = final_similarity_score
-
-
-        
-        top_k_mayores = dict(sorted(score_list.items(), key=lambda item: item[1], reverse=True)[:k])
+        top_k_mayores = get_top_candidates(job_id, k)
         
         return list(top_k_mayores.keys())
     except Exception as error:
         raise HTTPException(status_code=BAD_REQUEST, detail=str(error)) from error
+
+def get_top_candidates(job_id, k):
+    job_vector = index_jd.fetch(ids=[job_id], namespace="ns1")["vectors"].get(job_id)["values"]
+
+    n = round(2 * k, 0)
+
+    candidates = index_cv.query(vector=job_vector, include_values = True, top_k=int(n), namespace="ns1")
+
+    ids = {}
+    for candidate in candidates.get("matches"):
+        ids[candidate["id"]] = candidate["score"]
+
+    url = API_COMPANIES_URL + f"/companies/company/job_description_to_match/{job_id}/"
+    jd_data = requests.get(
+            url
+        )
+
+    jd_data = jd_data.json()
+
+    score_list = {}
+
+    dict_jd = JobDescriptionProcessor(' '.join([jd_data["title"], jd_data["description"], ' '.join(jd_data["responsabilities"]), ' '.join(jd_data["requirements"])])).process()
+
+        #print("job description: ", dict_jd)
+
+    jd_data["pos_frequencies"] = dict_jd["pos_frequencies"]
+    jd_data["keyterms"] = dict_jd["keyterms"]
+    jd_data["keywords_tfidf"] = dict_jd["keywords_tfidf"]
+
+    requirements_skills = DataExtractor(' '.join(jd_data['requirements'])).extract_skills()
+        
+    requirements_education = DataExtractor(' '.join(jd_data['requirements'])).extract_education_title()
+
+    print("requirements_skills: ", requirements_skills)
+    print("requirements_education: ", requirements_education)
+
+    for email, score in ids.items():
+        print("Candidato: ", email)
+        print("Score inicial: ", score)
+
+        url = API_USERS_URL + f"/users/user/resume/{email}/"
+        resume_fields = requests.get(
+                url
+            )
+            
+        resume_fields = resume_fields.json()
+
+            #print("resume_fields del candidato", resume_fields)
+        print("job_titles jd: ", jd_data['title'])
+        print("job titles resume: ", resume_fields["job_titles"])
+
+            # Job title match weight
+        job_title_weight = 1.2 if jd_data['title'].lower() in resume_fields["job_titles"] else 1.0
+
+        print("job_title_weight: ", job_title_weight)
+
+            # Requirements match weight
+        requirements_skills_weight = 1.0
+        print("skills: ", resume_fields["skills"])
+        print("model_data: ", resume_fields["model_data"])
+
+        for req in requirements_skills:
+            if req not in resume_fields["skills"] and req not in resume_fields["model_data"]:
+                requirements_skills_weight -= 0.1
+                if requirements_skills_weight < 0:
+                    requirements_skills_weight = 0
+                    break
+            elif req in resume_fields["skills"] or req in resume_fields["model_data"]:
+                requirements_skills_weight += 0.1
+
+        print("requirements_skills_weight: ", requirements_skills_weight)
+
+        requirements_education_weight = 1.0
+
+        cleaned_education = TextCleaner(resume_fields["education"]).clean_text()
+
+        for req in requirements_education:
+            if req not in cleaned_education and req not in resume_fields["model_data"]:
+                requirements_education_weight -= 0.1
+                if requirements_education_weight < 0:
+                    requirements_education_weight = 0
+                    break
+            elif req in cleaned_education or req in resume_fields["model_data"]:
+                requirements_education_weight += 0.1
+            
+        print("requirements_education_weight: ", requirements_education_weight)
+                    
+
+            # Pos frequencies weight
+        pos_freq_weight = 1.0
+        for word in jd_data['pos_frequencies'].keys():
+            if word in resume_fields["model_data"]:
+                pos_freq_weight += 0.1
+            
+        print("pos_freq_weight: ", pos_freq_weight)
+
+            # Keyterms and keywords_tfidf weight
+        keyterms_weight = 1.0
+        for term in jd_data['keyterms']:
+            if term[0] in resume_fields["model_data"]:
+                keyterms_weight += 0.1
+            
+        for keyword in jd_data['keywords_tfidf']:
+            if keyword in resume_fields["model_data"]:
+                keyterms_weight += 0.1
+
+        print("keyterms_weight: ", keyterms_weight)
+
+            
+
+        location_weight = 1.0
+
+        if jd_data["work_model"] == "on-site":
+            company_coordinates = get_coordinates_locationiq(jd_data["address"], API_LOCATION_KEY)
+            user_coordinates = get_coordinates_locationiq(resume_fields["address"], API_LOCATION_KEY)
+            distance_km = geodesic(user_coordinates, company_coordinates).kilometers
+            if distance_km > 50:
+                location_weight = 0.3
+            elif distance_km < 10:
+                location_weight = 1.2
+        elif jd_data["work_model"] == "hybrid":
+            company_coordinates = get_coordinates_locationiq(jd_data["address"], API_LOCATION_KEY)
+            user_coordinates = get_coordinates_locationiq(resume_fields["address"], API_LOCATION_KEY)
+            distance_km = geodesic(user_coordinates, company_coordinates).kilometers
+            if distance_km > 50:
+                location_weight = 0.6
+            elif distance_km < 10:
+                location_weight = 1.1
+
+        print("location_weight: ", location_weight)
+
+
+            #calculate age weight
+        age_weight = 1.0
+        if resume_fields["age"] < jd_data["age_range"][0]:
+            age_weight -= ((jd_data["age_range"][0] - resume_fields["age"]) * 0.04)
+
+        elif resume_fields["age"] > jd_data["age_range"][1]:    
+            age_weight -= ((resume_fields["age"] - jd_data["age_range"][1]) * 0.04)
+        else:
+            age_weight = 1.2
+
+        print("age_weight: ", age_weight)
+
+        job_preference_weight = 1.3
+
+        print("job_preferences: ", resume_fields["job_preferences"])
+            
+        if resume_fields["job_preferences"]:
+            job_preference_weight = 1.3 if jd_data['title'].lower() in resume_fields["job_preferences"] else 0.5
+
+        print("job_preference_weight: ", job_preference_weight)
+
+        years_of_experience_weight = 1.0
+        if resume_fields["years_of_experience"] < jd_data["years_of_experience"]:
+            years_of_experience_weight -= ((jd_data["years_of_experience"] - resume_fields["years_of_experience"]) * 0.04)
+
+            # Calculate final weighted similarity score
+        final_similarity_score = score * job_title_weight * requirements_skills_weight * requirements_education_weight * pos_freq_weight * keyterms_weight * location_weight * age_weight * job_preference_weight * years_of_experience_weight
+            
+        print("final_similarity_score: ", final_similarity_score)
+            
+        score_list[email] = final_similarity_score
+
+
+        
+    top_k_mayores = dict(sorted(score_list.items(), key=lambda item: item[1], reverse=True)[:k])
+    return top_k_mayores
 
 @router.delete("/matching/candidate/{email}/")
 def delete_candidate(email: str):
