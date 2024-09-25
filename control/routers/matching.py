00b055298d664d06f8b5d3607433a5425b1724d6
Martin <mscazzola@fi.uba.ar>
2024-09-25 20:49:41 -0300

    agrego print
    Co-authored-by: lara-scazzola1 <lscazzola@fi.uba.ar>

--- control/routers/matching.py
+++ control/routers/matching.py
@@ -71,6 +71,8 @@ def check_new_match(user_email):
     for job_id in job_ids[0]:
         top_candidates = get_top_candidates(job_id, AMOUNT_COMPANIES_CHECK)
 
+        print(f"top_candidates para id {job_id}: {top_candidates}")
+
         if top_candidates[0] == user_email:
             print("Match encontrado")
 
